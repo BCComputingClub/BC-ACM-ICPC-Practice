d8faa9790d27b251665656f25d8998600f939f33
5donuts <charliegerman32@gmail.com>
2016-10-15 16:18:25 -0400

    added problems for ACM Practice

--- ACM Practice/src/Project2.java
+++ ACM Practice/src/Project2.java
@@ -1,8 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Project2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = "";
+		String[] arr = new String[5];
+		double d; 
+		double h;
+		double z;
+		double n1;
+		double n2;
+		while((line = br.readLine()) != null)
+		{
+			arr = line.split(" ");
+			d = Double.parseDouble(arr[0]);
+			h = Double.parseDouble(arr[1]);
+			z = Double.parseDouble(arr[2]);
+			n1 = Double.parseDouble(arr[3]);
+			n2 = Double.parseDouble(arr[4]);
+			
+			
+		}
+			
 
 	}
 
